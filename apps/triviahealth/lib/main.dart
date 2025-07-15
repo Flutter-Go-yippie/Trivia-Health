7998b47afff1d29cdd7fe46f7cdb7bacfc135ccc
Ilyas Khatipov <ilyaskhat@mail.ru>
2025-07-15 13:58:19 +0300

    login/signup workflow work

--- apps/triviahealth/lib/main.dart
+++ apps/triviahealth/lib/main.dart
@@ -1,16 +1,27 @@
+import 'dart:io';
+
 import 'package:android_app/app/dependencies_factory.dart';
 import 'package:android_app/app/presentation/scopes/dependencies_scope.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'app/app_dependencies.dart';
 import 'app/app_router.dart';
 
+final _appRouter = AppRouter();
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.dependencies});
+
+  final AppDependencies dependencies;
 
   @override
   Widget build(BuildContext context) {
-    final appRouter = AppRouter();
-    final depScope = DependenciesFactory.build();
+    final appRouter = _appRouter;
+    final depScope = dependencies;
     return DependenciesScope(
       appDependencies: depScope,
       child: MaterialApp.router(
@@ -22,6 +33,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kDebugMode) print(Directory.current.path);
+  // await dotenv.load(fileName: '/home/blazz1t/Projects/NeuroCoach/apps/android-app/.env');
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final depScope = DependenciesFactory.build(prefs);
+  runApp(MyApp(dependencies: depScope));
 }
